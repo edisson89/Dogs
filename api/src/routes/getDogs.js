891d26d68a4b546e1251019fc7c9f505d678a345
edisson89 <edisson.12@hotmail.com>
2023-06-28 15:41:07 -0500

    Modularizando las rutas

--- api/src/routes/getDogs.js
+++ api/src/routes/getDogs.js
@@ -2,10 +2,18 @@ const express = require('express');
 const routerDog = express.Router();
 
 const getDogData = require('../controllers/obtenerDogs')
-const getDog = require('../controllers/dogGet')
+const getDogRaza = require('../controllers/dogGetRaza')
+const getDogName = require('../controllers/dogGetName')
+const dogPost = require('../controllers/dogPost')
+
+
 
 
 routerDog.get("/dogs", getDogData)
-routerDog.get("/:idRaza", getDog)
+routerDog.get("/:idRaza", getDogRaza)
+routerDog.get("/name", getDogName)
+routerDog.post("/dogs", dogPost)
+
+
 
 module.exports = routerDog
