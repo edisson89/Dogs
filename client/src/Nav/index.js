ac515219ce61e09a7b589d96d3461d060eab1e14
edisson89 <edisson.12@hotmail.com>
2023-06-30 08:37:00 -0500

    creando navlink

--- client/src/Nav/index.js
+++ client/src/Nav/index.js
@@ -1,27 +1,26 @@
-import React from 'react'
-import { NavLink } from 'react-router-dom'
-import styles from './nav.module.css'
+import React from "react";
+import { NavLink } from "react-router-dom";
+import styles from "./nav.module.css";
 
 const Nav = () => {
   return (
     <div className={styles.nav}>
-     <NavLink className={styles.navlink} to='/home' >
-      <button>Home</button>
-     </NavLink>
-     <NavLink className={styles.navlink} to='/form' >
-     <button>Form</button>
-     </NavLink>
-     <NavLink className={styles.navlink} to='/about' >
-     <button>About</button>
-     </NavLink>
-    
-     <NavLink className={styles.navlink} to='/Favorites' >
-     <button>Favorites</button>
-     </NavLink>
-     
+      
+      <NavLink className={styles.navlink} to="/home">
+        <button>Home</button>
+      </NavLink>
+      <NavLink className={styles.navlink} to="/form">
+        <button>Form</button>
+      </NavLink>
+      <NavLink className={styles.navlink} to="/about">
+        <button>About</button>
+      </NavLink>
 
+      <NavLink className={styles.navlink} to="/Favorites">
+        <button>Favorites</button>
+      </NavLink>
     </div>
-  )
-}
+  );
+};
 
-export default Nav
+export default Nav;
