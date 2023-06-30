86e73a79ff85d92c2041890df6aa36e67e528824
edisson89 <edisson.12@hotmail.com>
2023-06-30 08:38:18 -0500

    creando reducer

--- client/src/Redux/reducer.js
+++ client/src/Redux/reducer.js
@@ -1,13 +1,41 @@
+import { ADD_DATA, ADD_HOME, REMOVE_DATA, REMOVE_HOME } from "./actionsTypes";
 
-  
-  const initialState = {
-    home: [],
-    
-  };
-  
-  export default function rootReducer(state = initialState, { type, payload }) {
-    switch (type) {
-    
-    }
+const initialState = {
+  home: [],
+  data: [],
+};
+
+export default function rootReducer(state = initialState, { type, payload }) {
+  switch (type) {
+    case ADD_HOME:
+      return {
+        ...state,
+        home: [...state.home, payload],
+      };
+
+    case REMOVE_HOME:
+      const filterHome = state.home.filter((dog) => dog.name !== payload);
+      return {
+        ...state,
+        home: filterHome,
+      };
+
+    case ADD_DATA:
+      return {
+        ...state,
+        data: payload,
+      };
+
+    case REMOVE_DATA:
+      const filterData = state.data.filter((dog) => dog.name !== payload);
+      return {
+        ...state,
+        data: filterData,
+      };
+
+    default:
+      return {
+        ...state,
+      };
   }
-  
+}
