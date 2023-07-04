b6c8e482da0628049b5bee534856e1f79d721815
edisson89 <edisson.12@hotmail.com>
2023-07-03 22:05:49 -0500

    Creando inicio

--- client/src/Inicio/index.js
+++ client/src/Inicio/index.js
@@ -5,6 +5,8 @@ import styles from './inicio.module.css'
 const Inicio = () => {
   return (
     <div className={styles.inicio}>
+      <h1>WELCOME TO DOGS PAGE</h1>
+      <h3>A place  where your imagination will fly</h3>
       <NavLink to='/home'>
         <button className={styles.button}>Home</button>
       </NavLink>
