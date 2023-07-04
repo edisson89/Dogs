0e1a0f041ceaca40331cffa01e49a9e7bb8e3de2
edisson89 <edisson.12@hotmail.com>
2023-07-03 22:04:16 -0500

    creando about

--- client/src/About/index.js
+++ client/src/About/index.js
@@ -1,8 +1,29 @@
 import React from 'react'
+import styles  from './about.module.css'
 
 const About = () => {
   return (
-    <div>About</div>
+    <div className={styles.about}>
+      <h1>About Us</h1>
+      <br></br>
+      <p>
+      Acerca de los Perritos y Nuestro Logro de Darlos a Conocer
+
+En nuestro sitio web, nos apasiona el maravilloso mundo de los perritos y estamos comprometidos a compartir información valiosa sobre las diferentes razas caninas. Creemos que cada perrito es único y especial a su manera, y queremos ayudar a las personas a descubrir y comprender mejor a estos adorables compañeros peludos.
+
+Nuestro logro más destacado es poder presentarles a los amantes de los perros una amplia variedad de razas, cada una con sus características, temperamentos y necesidades específicas. Mediante nuestro trabajo de investigación exhaustiva y colaboración con expertos en el cuidado y entrenamiento de perros, hemos recopilado una extensa base de datos de razas caninas.
+
+En nuestro sitio web, encontrarás una sección dedicada a cada raza de perro, donde podrás explorar información detallada sobre su origen, tamaño, apariencia, temperamento, requisitos de cuidado y mucho más. Nuestro objetivo es proporcionar a los visitantes una visión completa y precisa de cada raza, para que puedan tomar decisiones informadas al elegir un nuevo miembro peludo para su familia.
+
+Además, hemos creado una galería de fotos con imágenes encantadoras de perros de diferentes razas. Creemos que las imágenes hablan más que mil palabras, y queremos que te enamores de la belleza y diversidad de los perros mientras exploras nuestra colección de fotografías.
+
+Nuestro compromiso no se detiene solo en la información estática. También ofrecemos una sección interactiva donde los usuarios pueden realizar búsquedas según sus preferencias y encontrar razas que se ajusten a sus necesidades específicas. ¡Puedes filtrar por tamaño, nivel de actividad, compatibilidad con niños y más!
+
+Estamos orgullosos de haber creado un recurso completo y accesible para que las personas aprendan sobre las razas de perros y tomen decisiones informadas al encontrar el compañero canino perfecto. Nuestro objetivo es promover la adopción responsable y el cuidado adecuado de los perros, fomentando relaciones felices y duraderas entre las personas y sus adorables peludos.
+
+¡Gracias por visitar nuestro sitio web y esperamos que disfrutes de tu experiencia mientras descubres el maravilloso mundo de las razas de perros!
+      </p>
+    </div>
   )
 }
 
