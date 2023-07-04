91cd4d6b1e3ce1f057482acf617d5616cd58323c
edisson89 <edisson.12@hotmail.com>
2023-07-03 22:05:06 -0500

    Creando detalles

--- client/src/CardDog/index.js
+++ client/src/CardDog/index.js
@@ -1,31 +1,23 @@
 import React from "react";
 import styles from "./cardog.module.css";
+import { NavLink } from "react-router-dom";
 
 const CardDog = ({
-  weight,
-  height,
   id,
   name,
   bred_for,
-  breed_group,
-  life_span,
-  origin,
   image,
 }) => {
   return (
     <div className={styles.card}>
+       <NavLink to={`/detail/${id}`}>
       <h1>{name}</h1>
       <h3>Bred for:{bred_for}</h3>
-      <h4>Weight:{weight.imperial}</h4>
-      <h4>Height:{height.imperial}</h4>
-      <h4>id:{id}</h4>
-      <h4>Breed group:{breed_group}</h4>
-      <h4>Life_span:{life_span}</h4>
-      <h4>Origin:{origin}</h4>
-      <div className={styles.image}>
+          <div className={styles.image}>
 
       <img alt={name} src={image} className={styles.img} />
       </div>
+      </NavLink>
     </div>
   );
 };
