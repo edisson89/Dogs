0d3fd5d7fa0d48e4abc50ae9c1c9a53fe4662d54
edisson89 <edisson.12@hotmail.com>
2023-07-04 21:56:03 -0500

    Añadiendo types al redducer

--- client/src/Redux/reducer.js
+++ client/src/Redux/reducer.js
@@ -1,18 +1,27 @@
-import { ADD_DATA, ADD_HOME, FILTER, ORDER, REMOVE_DATA, REMOVE_HOME, RESET } from "./actionsTypes";
+import {
+  ADD_DATA,
+  ADD_HOME,
+  FILTER,
+  ORDER,
+  REMOVE_DATA,
+  REMOVE_HOME,
+  RESET,
+} from "./actionsTypes";
 
 const initialState = {
   home: [],
-  homeOrigin:[],
-  data: []
+  homeOrigin: [],
+  data: [],
 };
 
 export default function rootReducer(state = initialState, { type, payload }) {
   switch (type) {
     case ADD_HOME:
+      
       return {
         ...state,
         home: [...state.home, payload],
-        homeOrigin:[...state.home, payload]
+        homeOrigin: [...state.home, payload],
       };
 
     case REMOVE_HOME:
@@ -20,34 +29,75 @@ export default function rootReducer(state = initialState, { type, payload }) {
       return {
         ...state,
         home: filterHome,
-        homeOrigin: filterHome
+        homeOrigin: filterHome,
       };
-      case FILTER:
-      const newFilter = state.home.filter(
-        (item) => item.name === payload
-      );
+    case FILTER:
+      const newFilter = state.homeOrigin.slice().sort((a, b) => {
+        if (payload === "name") {
+          const nameA = a.name.toLowerCase();
+          const nameB = b.name.toLowerCase();
+
+          if (nameA < nameB) {
+            return payload === "Ascendent" ? -1 : 1;
+          } else if (nameA > nameB) {
+            return payload === "Ascendent" ? 1 : -1;
+          } else {
+            return 0;
+          }
+        }
+
+        if (payload === "weigth") {
+          const nameA = a.name.toLowerCase();
+          const nameB = b.name.toLowerCase();
+
+          if (nameA < nameB) {
+            return payload === "weigth" ? -1 : 1;
+          } else if (nameA > nameB) {
+            return payload === "weigth" ? 1 : -1;
+          } else {
+            return 0;
+          }
+        }
+        if (payload === "origin") {
+          const nameA = a.name.toLowerCase();
+          const nameB = b.name.toLowerCase();
+
+          if (nameA < nameB) {
+            return payload === "origin" ? -1 : 1;
+          } else if (nameA > nameB) {
+            return payload === "origin" ? 1 : -1;
+          } else {
+            return 0;
+          }
+        } else return null;
+      });
+
       return {
         ...state,
         home: newFilter,
       };
+
     case ORDER:
-      const newOrder = state.homeOrigin.sort((a, b) => {
-        if (a.id > b.id) {
-          return "Ascendente" === payload ? 1 : -1;
-        }
+      const newOrder = state.homeOrigin.slice().sort((a, b) => {
         if (a.id < b.id) {
-          return "Descendente" === payload ? 1 : -1;
+          return payload === "Descendent" ? -1 : 1;
+        }
+        if (a.id > b.id) {
+          return payload === "Ascendent" ? -1 : 1;
         }
         return 0;
       });
+
       return {
         ...state,
-        myFavorites: newOrder,
+
+        home: newOrder,
       };
+
     case RESET:
       return {
         ...state,
-        home: [...state.homeOrigin],
+        home: state.homeOrigin,
       };
     case ADD_DATA:
       return {
