4e69fc6bacd922edf18c64d754e88b3a02fd1ca7
edisson89 <edisson.12@hotmail.com>
2023-07-04 21:54:29 -0500

    Agregando propiedes card

--- client/src/CardDog/index.js
+++ client/src/CardDog/index.js
@@ -7,12 +7,14 @@ const CardDog = ({
   name,
   bred_for,
   image,
+  weight
 }) => {
   return (
     <div className={styles.card}>
        <NavLink to={`/detail/${id}`}>
       <h1>{name}</h1>
       <h3>Bred for:{bred_for}</h3>
+      <h3>Weight:{weight.metric }</h3>
           <div className={styles.image}>
 
       <img alt={name} src={image} className={styles.img} />
