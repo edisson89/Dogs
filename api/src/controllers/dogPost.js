040e6b2c507b7e72f80358727450c882e21dd36d
edisson89 <edisson.12@hotmail.com>
2023-07-22 20:07:00 -0500

    actualizacion

--- api/src/controllers/dogPost.js
+++ api/src/controllers/dogPost.js
@@ -2,32 +2,16 @@ const { Dog } = require("../db");
 
 async function dogPost(req, res) {
   try {
-    const {
-      weight,
-      height,
-      id,
-      name,
-      bred_for,
-      breed_group,
-      life_span,
-      origin,
-      reference_image_id,
-      image,
-    } = req.query;
-    if (id,name,bred_for) {
+    const { weight, height, id, name, life_span } = req.query;
+    if ((id, name)) {
       const dog = {
         weight,
         height,
         id,
         name,
-        bred_for,
-        breed_group,
         life_span,
-        origin,
-        reference_image_id,
-        image,
       };
-      // Guarda los datos en la base de datos utilizando Sequelize
+      // add db
 
       await Dog.create(dog);
       return res.status(200).json(dog);
