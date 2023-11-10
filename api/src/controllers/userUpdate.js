27f7a898bfc93c7ecb2d31e8fbdd17b78b7b3d92
edisson89 <edisson.12@hotmail.com>
2023-11-10 11:22:07 -0500

    user

--- api/src/controllers/userUpdate.js
+++ api/src/controllers/userUpdate.js
@@ -3,16 +3,16 @@ const { User } = require("../db");
 const userUpdate = async (req, res) => {
   try {
     const { email, password } = req.body;
-    const validateUser = await User.findOne({
-      where: {email}
-    });
+      const validateUser = await User.findOne({
+        where: {email}
+      });
 
-    if (validateUser && validateUser.dataValues.password !== password) {
-        
-      const userUpdated = await validateUser.update({ password });
-      if (userUpdated) {
-       res.status(200).json({ update: userUpdated });
-      } else {
+      if (validateUser && validateUser.dataValues.password !== password) {
+          
+        const userUpdated = await validateUser.update({ password });
+        if (userUpdated) {
+        res.status(200).json({ update: userUpdated });
+        } else {
         res.status(404).json({ error: "User not found" });
       }
     }else {
