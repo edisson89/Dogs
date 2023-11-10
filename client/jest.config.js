0d71ff6b5a013cf4ac226d8f793f4d48002f23ed
edisson89 <edisson.12@hotmail.com>
2023-11-10 11:23:13 -0500

    settings jest

---
+++ client/jest.config.js
@@ -0,0 +1,9 @@
+import '@testing-library/jest-dom'
+/** @type {import('jest').Config} */
+const config = {
+    verbose: true,
+    setupFilesAfterEnv: ['<rootDir>/jest-setup.js']
+  };
+ 
+
+  module.exports = config;
