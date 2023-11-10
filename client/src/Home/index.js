bdf456865c8878b1810d5c2e36b4fa6110691546
edisson89 <edisson.12@hotmail.com>
2023-11-10 11:25:32 -0500

    add  states

--- client/src/Home/index.js
+++ client/src/Home/index.js
@@ -1,18 +1,15 @@
-import React, { useCallback, useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import styles from "./home.module.css";
 import axios from "axios";
 import { addData, addHome, filter, order, reset } from "../Redux/actions.js";
 import { useDispatch, useSelector } from "react-redux";
 import CardDog from "../CardDog";
- 
 
 const Home = () => {
   const [id, setId] = useState("");
-  const [loading,setLoading]= useState(false)
-  const [localDogs, setlocalDogs] = useState();
+  const [loading, setLoading] = useState(false);
   const dispatch = useDispatch();
   const dogs = useSelector((state) => state.home);
-  // const data = useSelector((state) => state.data);
 
   function handleChange(event) {
     event.preventDefault();
@@ -23,40 +20,57 @@ const Home = () => {
   useEffect(() => {
     async function onData() {
       try {
-        const { data } = await axios.get(
-          "http://localhost:3001/dogdata/dogs"
-        );
-        setLoading(true)
-        setlocalDogs(data.result);
-        dispatch(addData(data.result));
-        setLoading(false)
+        setLoading(true);
+        const { data } = await axios.get("http://localhost:3001/dogdata/dogs/");
+        if (data) {
+          dispatch(addData(data.result));
+        }
       } catch (error) {
-        console(error);
+        console.log(error.message);
       }
+      setLoading(false);
     }
     onData();
-    
-  }, [localDogs, dispatch]);
+  }, [dispatch]);
 
   //Add dogs to home por id
-  const onSearch = useCallback(async(id) =>{
-    try {
-      const { data } = await axios.get(
-        `http://localhost:3001/dogdata/dogs/${id}`
-      );
-      
-      dispatch(addHome(data.dbDog));
-    } catch (error) {
-      console.log(error);
-    }
-  },[dispatch]);
+ async function onSearch (id){
+      try {
+        console.log('se ejeecuto  mieerd')
+        const dataId = dogs.find((dog) => dog.id === id);
+        console.log(dataId)
+        if (!dataId) {
+          const { data } = await axios.get(
+            `http://localhost:3001/dogdata/dogs/${id}`
+          );
 
- 
- const handleOrder = (e) =>{
+          dispatch(addHome(data.dbDog));
+        } else {
+          alert("Data exist already");
+        }
+      } catch (error) {
+        console.log(error.message);
+      }
+    }
+  // useEffect(() => {
+  //   let isMounted = true;
+  //   onSearch(id).then(() => {
+  //     // Comprobamos si el componente sigue montado antes de actualizar el estado.
+  //     if (isMounted) {
+  //       setLoading(false);
+  //     }
+  //   });
+  //   // Clean-up para cancelar tareas asincrónicas cuando el componente se desmonte.
+  //   return () => {
+  //     isMounted = false;
+  //   };
+  // }, [id, onSearch]);
+
+  const handleOrder = (e) => {
     e.preventDefault();
     const { value } = e.target;
     dispatch(order(value));
-  }
+  };
   function handleFilter(e) {
     e.preventDefault();
     const { value } = e.target;
@@ -65,21 +79,18 @@ const Home = () => {
   function resetBtn() {
     dispatch(reset());
   }
-  useEffect((id) => {
-    onSearch(id)
-   }, [dogs,localDogs,onSearch,dispatch])
-   
+ 
   return (
     <div className={styles.home}>
       <div>
         <h1>Dogs</h1>
         <select onChange={handleOrder} name="order" defaultValue={"Default"}>
-        <option value="Default" disabled>
-          Select Order 
-        </option>
-        <option value="Ascendent">Ascendent</option>
-        <option value="Descendent">Descendent</option>
-      </select>
+          <option value="Default" disabled>
+            Select Order
+          </option>
+          <option value="Ascendent">Ascendent</option>
+          <option value="Descendent">Descendent</option>
+        </select>
 
         <select onChange={handleFilter} name="filter" defaultValue={"Default"}>
           <option value="Default" disabled>
@@ -90,7 +101,7 @@ const Home = () => {
           <option value="origin">Origin</option>
         </select>
 
-      <button onClick={resetBtn}>Reset</button>
+        <button onClick={resetBtn}>Reset</button>
         <input
           onChange={handleChange}
           type="search"
@@ -102,60 +113,46 @@ const Home = () => {
           onSearch{" "}
         </button>
       </div>
-      <div>
-     
-          { !loading ?(localDogs &&
-            localDogs
-              .map(
-                ({
-                  
-                  id,
-                  name,
-                  bred_for,
-                  image,
-                }) => {
-                  return (
-                    <CardDog
-                      key={id}                     
-                      id={id}
-                      name={name}
-                      bred_for={bred_for}
-                      image={image.url}
-                    />
-                  );
-                }
-              )
-              .reverse() ):(<div>loading...</div> )
-              }
-       
-      </div>
-      <div>
-        
-          {dogs &&
-            dogs
-              .map(
-                ({
-                  
-                  id,
-                  name,
-                  bred_for,
-                  image,
-                  weight
-                }) => {
-                  return (
-                    <CardDog
-                    key={id}                     
-                    id={id}
-                    name={name}
-                    bred_for={bred_for}
-                    image={image.url}
-                    weight={weight}
-                    />
-                  );
-                }
-              )
-              .reverse()}
-       
+      {/* <div>
+        {!loading ? (
+          localDogs &&
+          localDogs
+            .map(({ id, name, bred_for, image }) => {
+              return (
+                <CardDog
+                  key={id}
+                  id={id}
+                  name={name}
+                  bred_for={bred_for}
+                  image={image.url}
+                />
+              );
+            })
+            .reverse()
+        ) : (
+          <div>loading...</div>
+        )}
+      </div> */}
+      <div className={styles.homespin}>
+        {loading ? (
+          <div className={styles.spinner}></div>
+
+        ) : (
+          dogs
+            .map(({ id, name, bred_for, image, weight }) => {
+              return (
+                <CardDog
+                  key={id}
+                  id={id}
+                  name={name}
+                  bred_for={bred_for}
+                  image={image.url}
+                  weight={weight.metric}
+                />
+              );
+            })
+            .reverse()
+        )}
       </div>
     </div>
   );
