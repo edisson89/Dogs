5f8383c5ad93571d934b8d362486aa016a9bd15e
edisson89 <edisson.12@hotmail.com>
2023-11-10 11:26:58 -0500

    add actionTypes

--- client/src/Redux/actionsTypes.js
+++ client/src/Redux/actionsTypes.js
@@ -3,7 +3,11 @@ export const REMOVE_HOME ='REMOVE_HOME'
 export const FILTER = 'FILTER'
 export const ORDER = 'ORDER'
 export const RESET = 'RESET'
-
+export const ADD_DB = 'ADD_DB'
 
 export const ADD_DATA = 'ADD_DATA'
 export const REMOVE_DATA ='REMOVE_DATA'
+
+export const ADD_USER ='ADD_USER'
+export const REMOVE_USER ='REMOVE_USER'
+export const UPDATE_USER ='UPDATE_USER'
