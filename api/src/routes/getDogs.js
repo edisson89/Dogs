6a36195864d2bfe8cce514c7c5c48a1c3b56cf15
edisson89 <edisson.12@hotmail.com>
2023-11-10 11:22:52 -0500

    settings routes

--- api/src/routes/getDogs.js
+++ api/src/routes/getDogs.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const routerDog = express.Router();
 
-const getDogData = require('../controllers/obtenerDogs')
+const getDogDataHandler = require('../handlers/getDogDataHandler')
 const getDogRaza = require('../controllers/dogGetRaza')
 const getDogName = require('../controllers/dogGetName')
 const dogPost = require('../controllers/dogPost')
@@ -11,7 +11,7 @@ const dogPost = require('../controllers/dogPost')
 
 routerDog.get("/name", getDogName)
 routerDog.get("/:idRaza", getDogRaza)
-routerDog.get("/", getDogData)
+routerDog.get("/", getDogDataHandler)
 routerDog.post("/", dogPost)
 
 
