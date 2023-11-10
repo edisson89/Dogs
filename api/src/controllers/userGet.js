7e7f50a7dd971682527abad6dac7ba9f3163a9be
edisson89 <edisson.12@hotmail.com>
2023-11-10 11:21:39 -0500

    new  status

--- api/src/controllers/userGet.js
+++ api/src/controllers/userGet.js
@@ -8,12 +8,12 @@ const userGet  = async(req,res) => {
         const validateUser = await User.findOne({
             where:{email: email}});
         if(validateUser && validateUser.dataValues.email === email && validateUser.dataValues.password === password){
-            res.status(200).json({user: 'verificado'})
+            res.status(200).json({user: 'validated'})
         }else{
         res.status(404).json({validate:false})
         }
     }else{
-        res.status(404).json({error: 'Faltan datos'})
+        res.status(404).json({error: 'error not validaded'})
     }
     } catch (error) {
         response.status(404).json({error:error.message})
